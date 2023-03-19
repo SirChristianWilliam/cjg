1e890a150fc63bae1adee14d6826b77d359237e3
Chris Maki <c.william.developer@gmail.com>
2023-03-19 18:04:28 -0500

    Add some tertiary colors to actions/links, and notes.txt to catalogue where my heads at and how the project is going, override current bootstrap css properties (read the notes.txt to understand why)

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,7 +5,7 @@ import ProPic from "../ProPic/ProPic";
 import ColdwellLogo from "../ColdwellLogo/ColdwellLogo";
 import MobileNav from "../MobileNav/MobileNav";
 import DesktopNav from "../DesktopNav/DesktopNav";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import "./Nav.css";
 // 1. Nav bar has different render depending on viewport size. This should reflect in the classes
@@ -13,7 +13,7 @@ function Nav() {
   return (
     <>
       {/* Top banner */}
-      <nav className="bg-body-tertiary ">
+      <nav className="bg-body-tertiary bannerHardlyKnower ">
         <div className="container-fluid nav justify-content-start">
           <Phone />
           <Email />
