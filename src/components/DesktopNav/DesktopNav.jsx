ae0f7731f6b0dfbb8189c0b36d4813bb56e33ee4
Chris Maki <c.william.developer@gmail.com>
2023-03-19 19:51:06 -0500

    Add dropdown for the buyers tab on the desktop nav view

--- src/components/DesktopNav/DesktopNav.jsx
+++ src/components/DesktopNav/DesktopNav.jsx
@@ -20,21 +20,46 @@ function DesktopNav() {
             </svg>
           </a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
+
+        <li className="nav-item dropdown">
+          <a
+            className="btn btn-secondary dropdown-toggle nav-link"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            href="#"
+          >
             Buyers
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              className="bi bi-caret-down-fill"
+              className="bi bi-caret-down-fill-lg"
               viewBox="0 0 16 16"
             >
               <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
             </svg>
           </a>
+          <ul className="dropdown-menu">
+            <li>
+              <button className="dropdown-item" type="button">
+              Article for buyers
+              </button>
+            </li>
+            <li>
+              <button className="dropdown-item" type="button">
+              Article for buyers
+              </button>
+            </li>
+            <li>
+              <button className="dropdown-item" type="button">
+                Article for buyers
+              </button>
+            </li>
+          </ul>
         </li>
+
         <li className="nav-item">
           <a className="nav-link" href="#">
             Sellers
