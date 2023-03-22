a17b1b5ae6b78ef8798db51b2efe74c22bac20cf
Chris Maki <c.william.developer@gmail.com>
2023-03-21 20:01:48 -0500

    Toggle email icon on hover and active states

--- src/components/Email/Email.jsx
+++ src/components/Email/Email.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Email.css";
+
 function Email() {
+  const [icon, setIcon] = useState("mail");
+  const handleMouseEnter = () => {
+    setIcon("drafts");
+  };
+
+  const handleMouseLeave = () => {
+    setIcon("mail");
+  };
+
+  const handleClick = () => {
+    setIcon("drafts");
+  };
   return (
     <>
       <nav className="navbar bg-body-tertiary emailAndPhoneSizing">
         <div className="container">
-          <a href="mailto:cat@cbresults.com">
-            <span className="material-symbols-outlined ">mail</span>
+          <a
+            href="mailto:cat@cbresults.com"
+            onClick={handleClick}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            {icon === "mail" && (
+              <span className="material-symbols-outlined">mail</span>
+            )}
+            {icon === "drafts" && (
+              <span className="material-symbols-outlined">drafts</span>
+            )}
             <span className="phoneLineHeight"> Let's Chat!</span>
           </a>
         </div>
