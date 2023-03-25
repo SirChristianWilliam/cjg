b4ceced5f9a93ddf9a0889c90ad8b048a60f729b
Chris Maki <c.william.developer@gmail.com>
2023-03-24 20:24:56 -0500

    Add effect on desktop nav dropdown listings hover to hopefully indicate more effectively that a new tab will open. If not, then remove

--- src/components/DesktopNav/DesktopNav.jsx
+++ src/components/DesktopNav/DesktopNav.jsx
@@ -25,7 +25,7 @@ function DesktopNav() {
               <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
             </svg>
           </a>
-          <ul className="dropdown-menu">
+          <ul className="dropdown-menu" title="Opens in a new tab">
             <li>
               <a
                 className="dropdown-item"
