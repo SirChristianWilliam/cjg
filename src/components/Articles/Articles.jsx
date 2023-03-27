906a0f83ef115102eb1a5c729002cd28bb48c184
Chris Maki <c.william.developer@gmail.com>
2023-03-27 15:54:39 -0500

    Import 2 example article images for testing purposes. Add article paragraph text and card template code to be re-used for articles  (buyers as well as sellers too)

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -4,6 +4,36 @@ function Articles() {
   return (
     <>
       <h1>Articles</h1>
+      <p>This page will contain all articles that are separate from Buyers and Sellers. Unless directed otherwise. 
+        I'm not sure how many articles there will be yet, but I'll add in an example article component to start so 
+        it can be easily replicated in the future with minor adjustments where needed.
+      </p>
+      {/* <div class="card mb-3" style={{ maxWidth: "75%", margin: "auto" }}>
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img
+              src="/Images/chairs.jpg"
+              class="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p class="card-text">
+                <small class="text-body-secondary">
+                  Last updated 3 mins ago
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </>
   );
 }
