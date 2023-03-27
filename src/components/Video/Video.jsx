998cbb19dc1a38185ce854d078866e9ef10d4ea9
Chris Maki <c.william.developer@gmail.com>
2023-03-27 15:15:45 -0500

    Add Video component/jsx file for priming for later additions regarding iframe content.

---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,4 @@
+// https://getbootstrap.com/docs/5.3/helpers/ratio/
+
+// When adding videos, reference this link as it helps with the ratio sizing on iframes
+  // for responsive viewport and web/mobile designs.
