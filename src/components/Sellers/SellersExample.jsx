cb98b772da215320f6558d72b6352d878cf98ae9
Chris Maki <c.william.developer@gmail.com>
2023-04-02 13:16:31 -0500

    Replicate layout, responsiveness, and classes from buyers to the sellers articles

--- src/components/Sellers/SellersExample.jsx
+++ src/components/Sellers/SellersExample.jsx
@@ -3,94 +3,78 @@ import React from "react";
 function SellersExample() {
   return (
     <>
-      <div
-        className="card mb-3 d-flex buyerssellersmargintop"
-        style={{ maxWidth: "75%", margin: "auto" }}
-      >
-        <div className="row g-0 ">
-          <div className="col-md-4">
-            <img
-              src="Images/sandtime.png"
-              className="img-fluid"
-              //   rounded-start
-              alt="'Hourglass in the desert sand, the vibe is overcast and gloomy and cinematic'"
-            />
-            {/* <a href="#" className="stretched-link"></a> */}
-            {/* position-relative */}
+      <div className="card mb-3 d-flex flex-column align-items-center buyerssellersarticles ">
+        <img
+          src="Images/sandtime.png"
+          className="img-fluid buyerssellersimages"
+          alt="'Hourglass in the desert sand, the vibe is overcast and gloomy and cinematic'"
+        />
+
+        <div className="card-body w-100 buyerssellersarticletextcontainer">
+          <h5 className="card-title">What time is the right time to sell?</h5>
+          <div className="card-text">
+            <p className="card-text">
+              <br></br>
+              &nbsp; Selling a property is a significant decision that requires
+              careful consideration. One of the essential factors to keep in
+              mind is timing. Knowing when the right time to sell is can make a
+              significant difference in the sale price and how long it takes to
+              sell.
+              <br></br>
+              &nbsp; The ideal time to sell your property depends on various
+              factors such as market conditions, interest rates, seasonality,
+              and personal circumstances. Here are some key considerations to
+              keep in mind when determining the right time to sell.
+              <br></br>
+              <br></br>
+              &nbsp; 1. Market Conditions The real estate market is constantly
+              fluctuating, and market conditions can significantly impact the
+              sale price and how long it takes to sell. Understanding current
+              market trends, including supply and demand, can help you determine
+              whether it's a good time to sell. In a seller's market, where
+              demand is high and inventory is low, you may be able to sell your
+              property quickly and at a higher price. However, in a buyer's
+              market, where there's more inventory than demand, you may need to
+              lower your asking price or wait longer to find a buyer.
+              <br></br>
+              &nbsp; 2. Interest Rates Another factor that can impact the timing
+              of your sale is interest rates. When interest rates are low, it's
+              generally a good time to sell as buyers can take advantage of
+              lower mortgage rates, making homeownership more affordable. When
+              interest rates are high, it can make it more challenging for
+              buyers to afford a mortgage, potentially slowing down the sale
+              process.
+              <br></br>
+              &nbsp; 3. Seasonality The season can also impact the timing of
+              your sale. In general, spring and summer are the busiest seasons
+              for real estate, as many buyers prefer to move during the warmer
+              months. However, depending on your location and property type,
+              other seasons may be better suited to sell. For example, if you're
+              selling a ski-in/ski-out property, winter may be the best time to
+              sell.
+              <br></br>
+              &nbsp; 4. Personal Circumstances
+              <br></br>
+              &nbsp; Lastly, personal circumstances can also influence the
+              timing of your sale. If you're relocating for work or need to
+              downsize, the timing of your sale may be dictated by these
+              factors. It's essential to consider your personal circumstances
+              when determining when to sell.
+              <br></br>
+              <br></br>
+              &nbsp; In conclusion, determining the right time to sell is a
+              crucial factor in the sale price and how long it takes to sell.
+              Consider market conditions, interest rates, seasonality, and your
+              personal circumstances to determine when the best time to sell is.
+              With careful consideration and planning, you can maximize your
+              sale price and ensure a smooth and successful sale.
+            </p>
           </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">
-                What time is the right time to sell?
-              </h5>
-              <div className="card-text">
-                <p className="card-text">
-                  <br></br>
-                  &nbsp; Selling a property is a significant decision that
-                  requires careful consideration. One of the essential factors
-                  to keep in mind is timing. Knowing when the right time to sell
-                  is can make a significant difference in the sale price and how
-                  long it takes to sell.
-                  <br></br>
-                  &nbsp; The ideal time to sell your property depends on various
-                  factors such as market conditions, interest rates,
-                  seasonality, and personal circumstances. Here are some key
-                  considerations to keep in mind when determining the right time
-                  to sell.
-                  <br></br>
-                  <br></br>
-                  &nbsp; 1. Market Conditions The real estate market is
-                  constantly fluctuating, and market conditions can
-                  significantly impact the sale price and how long it takes to
-                  sell. Understanding current market trends, including supply
-                  and demand, can help you determine whether it's a good time to
-                  sell. In a seller's market, where demand is high and inventory
-                  is low, you may be able to sell your property quickly and at a
-                  higher price. However, in a buyer's market, where there's more
-                  inventory than demand, you may need to lower your asking price
-                  or wait longer to find a buyer.
-                  <br></br>
-                  &nbsp; 2. Interest Rates Another factor that can impact the
-                  timing of your sale is interest rates. When interest rates are
-                  low, it's generally a good time to sell as buyers can take
-                  advantage of lower mortgage rates, making homeownership more
-                  affordable. When interest rates are high, it can make it more
-                  challenging for buyers to afford a mortgage, potentially
-                  slowing down the sale process.
-                  <br></br>
-                  &nbsp; 3. Seasonality The season can also impact the timing of
-                  your sale. In general, spring and summer are the busiest
-                  seasons for real estate, as many buyers prefer to move during
-                  the warmer months. However, depending on your location and
-                  property type, other seasons may be better suited to sell. For
-                  example, if you're selling a ski-in/ski-out property, winter
-                  may be the best time to sell.
-                  <br></br>
-                  &nbsp; 4. Personal Circumstances
-                  <br></br>
-                  &nbsp; Lastly, personal circumstances can also influence the
-                  timing of your sale. If you're relocating for work or need to
-                  downsize, the timing of your sale may be dictated by these
-                  factors. It's essential to consider your personal
-                  circumstances when determining when to sell.
-                  <br></br>
-                  <br></br>
-                  &nbsp; In conclusion, determining the right time to sell is a
-                  crucial factor in the sale price and how long it takes to
-                  sell. Consider market conditions, interest rates, seasonality,
-                  and your personal circumstances to determine when the best
-                  time to sell is. With careful consideration and planning, you
-                  can maximize your sale price and ensure a smooth and
-                  successful sale.
-                </p>
-              </div>
-              <p className="card-text">
-                <small className="text-body-secondary">
-                  Last updated March 27th, 2023
-                </small>
-              </p>
-            </div>
-          </div>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              Last updated March 27th, 2023
+            </small>
+          </p>
         </div>
       </div>
     </>
--- src/components/Sellers/SellersExample2.jsx
+++ src/components/Sellers/SellersExample2.jsx
@@ -3,94 +3,80 @@ import React from "react";
 function SellersExample2() {
   return (
     <>
-      <div
-        className="card mb-3 d-flex buyerssellersmargintop"
-        style={{ maxWidth: "75%", margin: "auto" }}
-      >
-        <div className="row g-0 ">
-          <div className="col-md-4">
-            <img
-              src="Images/cybersecurity.png"
-              className="img-fluid"
-              //   rounded-start
-              alt="'Anonymous the organization, white masked character with a creepy look and dark and insidious feeling tech background' "
-            />
-            {/* <a href="#" className="stretched-link"></a> */}
-            {/* position-relative */}
+      <div className="card mb-3 d-flex flex-column align-items-center buyerssellersarticles ">
+        <img
+          src="Images/cybersecurity.png"
+          className="img-fluid buyerssellersimages"
+          //   rounded-start
+          alt="'Anonymous the organization, white masked character with a creepy look and dark and insidious feeling tech background' "
+        />
+        <div className="card-body w-100 buyerssellersarticletextcontainer">
+          <h5 className="card-title">
+            Look out for these common scams when selling your home.
+          </h5>
+          <div className="card-text">
+            <p className="card-text">
+              <br></br>
+              Selling a home can be a daunting task, but unfortunately, it can
+              also make you vulnerable to scammers. Scammers are known for
+              targeting homeowners who are looking to sell their property, and
+              it's essential to be aware of the common scams to protect yourself
+              from falling victim to them. Here are some of the most common
+              scams to look out for when selling your home.
+              <br></br>
+              <br></br>
+              &nbsp; 1. Phishing Scams Phishing scams are prevalent in the real
+              estate industry, and they involve scammers posing as real estate
+              agents, title companies, or other legitimate entities to steal
+              personal and financial information. These scammers may send emails
+              or make phone calls requesting personal information, such as
+              social security numbers or bank account details. It's crucial to
+              be cautious when providing personal information and to verify the
+              legitimacy of any requests.
+              <br></br>
+              &nbsp; 2. Wire Transfer Scams Wire transfer scams are another
+              common scam in the real estate industry. In this type of scam,
+              scammers pose as real estate agents or title companies and request
+              wire transfers for closing costs or other expenses. The scammers
+              provide fake wire transfer instructions, and once the money is
+              transferred, it's nearly impossible to recover. It's important to
+              verify wire transfer instructions with your bank or title company
+              and to be cautious when sending large sums of money.
+              <br></br>
+              &nbsp; 3. Fake Cashier's Check Scams Fake cashier's check scams
+              involve scammers sending fake cashier's checks to homeowners and
+              requesting that they deposit them and send a portion of the funds
+              back. The checks appear to be legitimate, but they're actually
+              counterfeit. Once the check is deposited, the homeowner sends the
+              requested funds, and then the bank discovers the check is fake,
+              leaving the homeowner responsible for the full amount. It's
+              important to verify the authenticity of any cashier's check and to
+              be cautious when sending funds back to a buyer.
+              <br></br>
+              &nbsp; 4. Identity Theft Scams Identity theft scams are another
+              common scam in the real estate industry. These scams involve
+              scammers stealing personal information, such as social security
+              numbers or credit card details, and using it to make fraudulent
+              purchases or open new accounts. It's important to be cautious when
+              providing personal information and to monitor your accounts for
+              any unauthorized activity.
+              <br></br>
+              <br></br>
+              &nbsp; In conclusion, it's important to be aware of the common
+              scams in the real estate industry when selling your home. Look out
+              for phishing scams, wire transfer scams, fake cashier's check
+              scams, and identity theft scams. Be cautious when providing
+              personal information, verify wire transfer instructions and
+              cashier's checks, and monitor your accounts for any unauthorized
+              activity. By being aware of these common scams, you can protect
+              yourself and ensure a successful sale.
+            </p>
           </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">
-                Look out for these common scams when selling your home.
-              </h5>
-              <div className="card-text">
-                <p className="card-text">
-                  <br></br>
-                  Selling a home can be a daunting task, but unfortunately, it
-                  can also make you vulnerable to scammers. Scammers are known
-                  for targeting homeowners who are looking to sell their
-                  property, and it's essential to be aware of the common scams
-                  to protect yourself from falling victim to them. Here are some
-                  of the most common scams to look out for when selling your
-                  home.
-                  <br></br>
-                  <br></br>
-                  &nbsp; 1. Phishing Scams Phishing scams are prevalent in the
-                  real estate industry, and they involve scammers posing as real
-                  estate agents, title companies, or other legitimate entities
-                  to steal personal and financial information. These scammers
-                  may send emails or make phone calls requesting personal
-                  information, such as social security numbers or bank account
-                  details. It's crucial to be cautious when providing personal
-                  information and to verify the legitimacy of any requests.
-                  <br></br>
-                  &nbsp; 2. Wire Transfer Scams Wire transfer scams are another
-                  common scam in the real estate industry. In this type of scam,
-                  scammers pose as real estate agents or title companies and
-                  request wire transfers for closing costs or other expenses.
-                  The scammers provide fake wire transfer instructions, and once
-                  the money is transferred, it's nearly impossible to recover.
-                  It's important to verify wire transfer instructions with your
-                  bank or title company and to be cautious when sending large
-                  sums of money.
-                  <br></br>
-                  &nbsp; 3. Fake Cashier's Check Scams Fake cashier's check
-                  scams involve scammers sending fake cashier's checks to
-                  homeowners and requesting that they deposit them and send a
-                  portion of the funds back. The checks appear to be legitimate,
-                  but they're actually counterfeit. Once the check is deposited,
-                  the homeowner sends the requested funds, and then the bank
-                  discovers the check is fake, leaving the homeowner responsible
-                  for the full amount. It's important to verify the authenticity
-                  of any cashier's check and to be cautious when sending funds
-                  back to a buyer.
-                  <br></br>
-                  &nbsp; 4. Identity Theft Scams Identity theft scams are
-                  another common scam in the real estate industry. These scams
-                  involve scammers stealing personal information, such as social
-                  security numbers or credit card details, and using it to make
-                  fraudulent purchases or open new accounts. It's important to
-                  be cautious when providing personal information and to monitor
-                  your accounts for any unauthorized activity.
-                  <br></br>
-                  <br></br>
-                  &nbsp; In conclusion, it's important to be aware of the common
-                  scams in the real estate industry when selling your home. Look
-                  out for phishing scams, wire transfer scams, fake cashier's
-                  check scams, and identity theft scams. Be cautious when
-                  providing personal information, verify wire transfer
-                  instructions and cashier's checks, and monitor your accounts
-                  for any unauthorized activity. By being aware of these common
-                  scams, you can protect yourself and ensure a successful sale.
-                </p>
-              </div>
-              <p className="card-text">
-                <small className="text-body-secondary">
-                  Last updated March 27th, 2023
-                </small>
-              </p>
-            </div>
-          </div>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              Last updated March 27th, 2023
+            </small>
+          </p>
         </div>
       </div>
     </>
